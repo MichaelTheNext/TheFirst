cd90eb2b4e10e19172de50c2c5428ea122a5ab90
Michael <Mustangthekill@gmail.com>
2017-07-18 16:40:01 +0300

    New folder c#

---
+++ FirstProdject/FirstProdject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProdject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow world!");
+            string s;
+            s = Console.ReadLine();
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
